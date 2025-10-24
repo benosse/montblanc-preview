29508b024eb05e38d0f13f7fb958b1ca077e6340
benoit <benoit.zenker@gmail.com>
2025-10-24 12:35:05 +0200

    Remove github pages code

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-const repo = 'montblanc-preview'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-  plugins: [svelte()],
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-})
+  plugins: [
+    svelte()
+  ],
+});
